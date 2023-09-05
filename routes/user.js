37e3184cb1a90dd5057bee82f9682262c249a26f
Aditya Raj Singh <adi4adityaraj@gmail.com>
2023-09-05 10:46:46 +0530

    user

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const { ensureAuth, ensureGuest, ensureAdmin } = require("../middleware/auth");
 const userModel = require("../models/User");
+const userSkillModel = require("../models/UserSkill");
 
 // Read a user by ID
 router.get("/:userId", ensureAuth, (req, res) => {
   const id = req.params.userId;
+  let userRecord = {};
   userModel
     .findById(id)
     .select("-__v -googleId -createdAt")
@@ -13,7 +15,20 @@ router.get("/:userId", ensureAuth, (req, res) => {
       if (!user) {
         return res.status(404).json({ error: "User not found" });
       }
-      res.json(user).status(200);
+      userRecord = user;
+      // res.json(user).status(200);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+
+  // Get user skills
+  userSkillModel
+    .find({ userId: id })
+    .exec()
+    .then((skills) => {
+      userRecord.skills = skills;
+      res.json(userRecord).status(200);
     })
     .catch((error) => {
       res.status(500).json({ error: error.message });
@@ -31,7 +46,7 @@ router.put("/:userId", ensureAuth, (req, res) => {
       if (!user) {
         return res.status(404).json({ error: "User not found" });
       }
-      res.status(200).json(user);
+      res.status(200).json(uer);
     })
     .catch((error) => {
       res.status(500).json({ error: error.message });
