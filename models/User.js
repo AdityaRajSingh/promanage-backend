7bc41206561d81bd114e068a789ded9a4c66f538
dakshbhardwaj <dakshbhardwaj2@gmail.com>
2023-09-07 19:52:40 +0530

    added admin flag

--- models/User.js
+++ models/User.js
@@ -34,6 +34,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
